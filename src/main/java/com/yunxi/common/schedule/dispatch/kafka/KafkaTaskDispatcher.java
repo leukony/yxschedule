247b69e80f579731a711562662922a87cf792582
leukony <leukony@yeah.net>
2019-06-21 20:24:11 +0800

    初始化

---
+++ src/main/java/com/yunxi/common/schedule/dispatch/kafka/KafkaTaskDispatcher.java
@@ -0,0 +1,10 @@
+package com.yunxi.common.schedule.dispatch.kafka;
+
+/**
+ * 
+ * @author <a href="mailto:leukony@yeah.net">leukony</a>
+ * @version $Id: KafkaTaskDispatcher.java, v 0.1 2019年6月21日 下午8:23:42 leukony Exp $
+ */
+public class KafkaTaskDispatcher {
+
+}
