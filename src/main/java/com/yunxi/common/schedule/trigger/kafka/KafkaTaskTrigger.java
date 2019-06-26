675ff8cb4e8a7a2da0acdbe8bdf460510b2e2653
leukony <leukony@yeah.net>
2019-06-26 17:29:21 +0800

    增加配置

--- src/main/java/com/yunxi/common/schedule/trigger/kafka/KafkaTaskTrigger.java
+++ src/main/java/com/yunxi/common/schedule/trigger/kafka/KafkaTaskTrigger.java
@@ -9,16 +9,25 @@ import org.springframework.kafka.listener.MessageListener;
 
 import com.yunxi.common.schedule.TaskManager;
 import com.yunxi.common.schedule.model.TaskItem;
+import com.yunxi.common.schedule.trigger.TaskTrigger;
 
 /**
  * 基于kafka的定时任务触发器.
  * @author <a href="mailto:leukony@yeah.net">leukony</a>
  * @version $Id: KafkaTaskTrigger.java, v 0.1 2019年6月20日 下午1:56:08 leukony Exp $
  */
-public class KafkaTaskTrigger implements MessageListener<String, Map<Object, Object>> {
+public class KafkaTaskTrigger implements TaskTrigger, MessageListener<String, Map<Object, Object>> {
 
     /** 定时任务服务  */
     private TaskManager taskManager;
+
+    /** 
+     * @see com.yunxi.common.schedule.trigger.TaskTrigger#registerTask(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void registerTask(String groupId, String taskName) {
+        // ignore
+    }
 
     /** 
      * @see org.springframework.kafka.listener.GenericMessageListener#onMessage(java.lang.Object)
